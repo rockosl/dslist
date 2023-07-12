d7863c4533f45f8cd759a0c7c81ba6b67a800d29
Ricardo <rockobaschirotto@gmail.com>
2023-07-11 22:04:41 -0300

    implementado get by list

--- src/main/java/com/rocko/dslist/dto/GameMinDTO.java
+++ src/main/java/com/rocko/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.rocko.dslist.dto;
 
 import com.rocko.dslist.entities.Game;
+import com.rocko.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -19,6 +20,14 @@ public class GameMinDTO {
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
+	}
+
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
--- src/main/java/com/rocko/dslist/services/GameService.java
+++ src/main/java/com/rocko/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.rocko.dslist.dto.GameDTO;
 import com.rocko.dslist.dto.GameMinDTO;
 import com.rocko.dslist.entities.Game;
+import com.rocko.dslist.projections.GameMinProjection;
 import com.rocko.dslist.repositories.GameRepository;
 
 @Service
@@ -28,5 +29,11 @@ public class GameService {
 		var result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
